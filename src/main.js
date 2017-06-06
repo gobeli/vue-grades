c4a928886e0b617bf3c136d2deff6990342d4df5
Etienne Gobeli <etienne.gobeli@students.fhnw.ch>
2017-06-06 12:23:41 +0200

    fix module handling

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+require('vue-material/dist/vue-material.css');
+require('./styles/styles.scss');
+
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
-import 'vue-material/dist/vue-material.css'
 
 import router from './router';
 import firebase from './services/firebase';
 import App from './components/app/app';
-import './styles/styles.scss';
 
 Vue.config.productionTip = false
 
